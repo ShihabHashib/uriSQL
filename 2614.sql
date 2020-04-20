273c4c1bd05234417d300c5b1b429cd0c0235477
shihabhashib <shihabhashib1@gmail.com>
2020-04-20 08:21:41 +0600

    2613, 1614 added

---
+++ 2614.sql
@@ -0,0 +1,4 @@
+SELECT customers.name, rentals.rentals_date 
+FROM customers
+INNER JOIN rentals ON customers.id = rentals.id_customers 
+WHERE rentals.rentals_date >='2016-09-01' and rentals.rentals_date<='2016-09-30';
